00baf6ad4f170b1fdb628ebe4d2148f64c5e3245
caporatang <caporatang@naver.com>
2024-12-20 17:53:52 +0900

    [baekjoon] 나머지_3052

---
+++ src/main/java/baekjoon/나머지_3052.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * packageName : baekjoon
+ * fileName : 나머지_3052
+ * author : taeil
+ * date : 2024. 12. 21.
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2024. 12. 21.        taeil                   최초생성
+ */
+public class 나머지_3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr= new int[10];
+        for (int i = 0; i <9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i <arr.length; i++) {
+            hs.add(arr[i]%42);
+        }
+        System.out.println(hs.size());
+    }
+}
