05a5108262433834d4b0830ba8bf1eda71e83bc8
caporatang <caporatang@naver.com>
2024-12-22 19:21:35 +0900

    Add : [baekjoon] 단어_길이_재기_2743

---
+++ src/main/java/baekjoon/strings/단어_길이_재기_2743.java
@@ -0,0 +1,24 @@
+package baekjoon.strings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * packageName : baekjoon.strings
+ * fileName : 단어_길이_재기
+ * author : taeil
+ * date : 2024. 12. 22.
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2024. 12. 22.        taeil                   최초생성
+ */
+public class 단어_길이_재기_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s.length());
+    }
+}
