5242e5a997f21a407b4420e9fe1b37e8953d37e9
caporatang <caporatang@naver.com>
2024-12-24 18:00:23 +0900

    Add : [baekjoon] 알고리즘_수업_알고리즘의_수행_시간_2_24263

---
+++ src/main/java/baekjoon/timeComplexity/알고리즘_수업_알고리즘의_수행_시간_2_24263.java
@@ -0,0 +1,25 @@
+package baekjoon.timeComplexity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * packageName : baekjoon.timeComplexity
+ * fileName : 알고리즘_수업_알고리즘의_수행_시간_2_24263
+ * author : taeil
+ * date : 2024. 12. 24.
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2024. 12. 24.        taeil                   최초생성
+ */
+public class 알고리즘_수업_알고리즘의_수행_시간_2_24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
